e5ba54f3ca3b8ae11b61b9aa5892c0e214fc9ca9
julianandres42 <julianandres42@gmail.com>
2021-04-20 22:40:17 -0500

    add shape type

--- game/shape.go
+++ game/shape.go
@@ -1,7 +1,19 @@
 package game
 
-type Shape interface {
-	moveLeft()
-	moveRight()
-	rotate()
+type Position struct {
+	x int
+	y int
 }
+
+type Shape struct {
+	positions       []*Position
+	currentRotation int
+	maxRotations    int
+	rotate          func(positions []*Position) []*Position
+}
+
+func (shape *Shape) rotateShape(){
+	shape.positions = shape.rotate(shape.positions)
+}
+
+
