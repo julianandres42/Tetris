48a9aaf81373f2d935aacc6ff046499a642dcaa2
julianandres42 <julianandres42@gmail.com>
2021-10-06 22:34:36 -0500

    add evaluate lines

--- board/board.go
+++ board/board.go
@@ -27,3 +27,33 @@ func (board *Board) Init(lenght, height int) {
 		initSquare.Y -= 40
 	}
 }
+
+func (board *Board) EvaluateLines() {
+	for i, row := range board.Matrix {
+		if isLine(row) {
+			board.downRows(i)
+		}
+	}
+}
+
+func isLine(cellRow []*Cell) bool {
+	for _, cell := range cellRow {
+		if !cell.active {
+			return false
+		}
+	}
+	return true
+}
+
+func (board *Board) downRows(rowIndex int) {
+	for i := rowIndex; i >= 1; i-- {
+		switchCellValues(board.Matrix[i-1], board.Matrix[i])
+	}
+}
+
+func switchCellValues(source []*Cell, destiny []*Cell) {
+	for i := range source {
+		destiny[i].active = source[i].active
+		source[i].active = false
+	}
+}
--- board/board_test.go
+++ board/board_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestInitBoard(t *testing.T) {
+func TestBoard_Init(t *testing.T) {
 	board := &Board{}
 	board.Init(20, 10)
 	if len(board.Matrix) != 20 {
@@ -22,5 +22,29 @@ func TestInitBoard(t *testing.T) {
 	}
 	if board.Matrix[19][9].Square.Y != 87 {
 		t.Errorf(fmt.Sprintf("Got %d, expected %d", board.Matrix[19][9].Square.Y, 87))
+	}
+}
+
+func TestBoard_EvaluateLines(t *testing.T) {
+	board := &Board{}
+	board.Init(20, 10)
+	setLine(board.Matrix[5])
+	setLine(board.Matrix[10])
+	setLine(board.Matrix[15])
+	board.EvaluateLines()
+	if isLine(board.Matrix[5]) {
+		t.Errorf(fmt.Sprintf("line found in position %d", 5))
+	}
+	if isLine(board.Matrix[10]) {
+		t.Errorf(fmt.Sprintf("line found in position %d", 10))
+	}
+	if isLine(board.Matrix[15]) {
+		t.Errorf(fmt.Sprintf("line found in position %d", 15))
+	}
+}
+
+func setLine(cells []*Cell) {
+	for _, cell := range cells {
+		cell.active = true
 	}
 }
