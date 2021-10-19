1442e690940dfd1a043b56707a19861c69bab110
julianandres42 <julianandres42@gmail.com>
2021-10-18 22:33:28 -0500

    fixing test for shape factory

--- shape/shape_factory_test.go
+++ shape/shape_factory_test.go
@@ -4,7 +4,24 @@ import "testing"
 
 func TestShapeFactory_getSquare(t *testing.T) {
 	square := shapeFct[int(Cube)]()
+	square.InitializeShape(1)
 	if square.ShapeType != Cube {
 		t.Errorf("Bad shape type, got %d, expected %d", square.ShapeType, Cube)
+	}
+}
+
+func TestShapeFactory_getEle(t *testing.T) {
+	square := shapeFct[int(Ele)]()
+	square.InitializeShape(1)
+	if square.ShapeType != Ele {
+		t.Errorf("Bad shape type, got %d, expected %d", square.ShapeType, Ele)
+	}
+}
+
+func TestShapeFactory_getCube(t *testing.T) {
+	square := shapeFct[int(Bar)]()
+	square.InitializeShape(1)
+	if square.ShapeType != Bar {
+		t.Errorf("Bad shape type, got %d, expected %d", square.ShapeType, Bar)
 	}
 }
