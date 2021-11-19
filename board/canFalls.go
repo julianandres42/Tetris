39d7c9b70de7cdc3f3e6b4afdb4f63db22219783
julianandres42 <julianandres42@gmail.com>
2021-11-18 22:26:33 -0500

    fix can fall logic and unit testing

--- board/canFalls.go
+++ board/canFalls.go
@@ -4,7 +4,9 @@ type canFallFunctionMapper map[int]func([]*Position, [][]*Cell) bool
 
 var mapperFalls = canFallFunctionMapper{
 	int(Cube): func(positions []*Position, matrix [][]*Cell) bool {
-		return (!matrix[positions[2].GetY()+1][positions[2].GetX()].Active && !matrix[positions[3].GetY()+1][positions[3].GetX()].Active) ||
-			(positions[2].GetY()+1 <= len(matrix)-1 && positions[3].GetY()+1 <= len(matrix)-1)
+		return (positions[1].GetY()+1 <= len(matrix)-1) &&
+			(!matrix[positions[1].GetY()+1][positions[1].GetX()].Active &&
+				!matrix[positions[3].GetY()+1][positions[3].GetX()].Active)
+
 	},
 }
--- board/canFalls_test.go
+++ board/canFalls_test.go
@@ -20,9 +20,29 @@ func TestShape_CanNotFallOneCellDown(t *testing.T) {
 	board.Init(20, 10)
 	square := ShapeFct[int(Cube)]()
 	square.InitializeShape(5)
-	board.Matrix[square.Positions[2].GetY()+1][square.Positions[2].GetX()].Active = true
+	board.Matrix[square.Positions[1].GetY()+1][square.Positions[1].GetX()].Active = true
 	funcFall := mapperFalls[int(Cube)]
-	if !funcFall(square.Positions, board.Matrix) {
+	if funcFall(square.Positions, board.Matrix) {
+		t.Errorf("Bad position")
+	}
+	board.Matrix[square.Positions[1].GetY()+1][square.Positions[1].GetX()].Active = false
+	board.Matrix[square.Positions[3].GetY()+1][square.Positions[3].GetX()].Active = true
+	if funcFall(square.Positions, board.Matrix) {
+		t.Errorf("Bad position")
+	}
+}
+
+func TestCanNotFallBoardBorder(t *testing.T) {
+	board := &Board{}
+	board.Init(20, 10)
+	square := ShapeFct[int(Cube)]()
+	square.InitializeShape(5)
+	for _, pos := range square.Positions {
+		pos.Y += 18
+	}
+	funcFall := mapperFalls[int(Cube)]
+	board.UpdateShape(square, true)
+	if funcFall(square.Positions, board.Matrix) {
 		t.Errorf("Bad position")
 	}
 }
