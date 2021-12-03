8071735b2a73df23fe2503dd90814b67b836d6a4
julianandres42 <julianandres42@gmail.com>
2021-12-02 22:27:38 -0500

    starting playing system

--- game/game_starter.go
+++ game/game_starter.go
@@ -3,6 +3,7 @@ package game
 import (
 	board2 "Tetris/board"
 	"golang.org/x/image/colornames"
+	"image/color"
 )
 
 type Game struct {
@@ -28,6 +29,7 @@ func (game *Game) Play() {
 	game.CurrentShape = board2.ShapeFct[int(board2.Cube)]()
 	game.CurrentShape.InitializeShape(5)
 	game.Drawer.DrawShape(game.Board, game.CurrentShape, colornames.Yellow)
+	go playing(game)
 }
 
 func (game *Game) UpdateScreen() {
@@ -36,4 +38,36 @@ func (game *Game) UpdateScreen() {
 
 func (game *Game) UpdateScreenNoClose() {
 	game.Drawer.UpdateScreenNoClose()
+}
+
+func playing(game *Game) {
+	for {
+		if !game.CurrentShape.CanFall(game.Board.Matrix) {
+			break
+		} else {
+			previousPositions := clonePositions(game.CurrentShape.Positions)
+			game.CurrentShape.Fall()
+			actualPositions := game.CurrentShape.Positions
+			updateShape(game, previousPositions, false, colornames.Grey)
+			updateShape(game, actualPositions, true, colornames.Yellow)
+			game.UpdateScreenNoClose()
+		}
+	}
+}
+
+func updateShape(game *Game, positions []*board2.Position, truth bool, color color.Color) {
+	game.CurrentShape.Positions = positions
+	game.Board.UpdateShape(game.CurrentShape, truth)
+	game.Drawer.DrawShape(game.Board, game.CurrentShape, color)
+}
+
+func clonePositions(positions []*board2.Position) []*board2.Position {
+	newPositions := make([]*board2.Position, len(positions))
+	for index, element := range positions {
+		newPositions[index] = &board2.Position{
+			X: element.X,
+			Y: element.Y,
+		}
+	}
+	return newPositions
 }
